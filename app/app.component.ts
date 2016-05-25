f5411ab36e6d48ac489c0649517f259c0c4f00d8
DeronLee <akagilnc@gmail.com>
2016-05-25 15:42:12 +0800

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by deronlee on 5/23/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+
+export class AppComponent{}
+
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by deronlee on 5/25/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
