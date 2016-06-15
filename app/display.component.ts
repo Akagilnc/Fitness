95684021e5cc24fb6535b6933c154337580225f0
DeronLee <akagilnc@gmail.com>
2016-06-15 14:54:15 +0800

    chart test2

--- app/display.component.ts
+++ app/display.component.ts
@@ -33,20 +33,24 @@ export class DisplayComponent implements OnInit {
         this.getData()
     }
 
-    public barChartOptions:any = {
-        scaleShowVerticalLines: false,
-        responsive: true
-    };
-    public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    public barChartType:string = 'bar';
-    public barChartLegend:boolean = true;
-
-    public barChartDatas:any[] = [
-        {data: [65, 59, 80, 81, 56, 55, 40], label:'Series A'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label:'Series B'}
+    // lineChart
+    public lineChartData:Array<any> = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
     ];
+    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartType:string = 'line';
+    public pieChartType:string = 'pie';
 
-    // events
+    // Pie
+    public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+    public pieChartData:number[] = [300, 500, 100];
+
+    public randomizeType():void {
+        this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
+        this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
+    }
+
     public chartClicked(e:any):void {
         console.log(e);
     }
