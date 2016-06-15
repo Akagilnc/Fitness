f3b881211b9fafe3a3b41dae3e43c310f9281fd7
DeronLee <akagilnc@gmail.com>
2016-06-15 14:50:11 +0800

    chart test

--- app/display.component.ts
+++ app/display.component.ts
@@ -4,22 +4,24 @@
 
 import {Component, ViewChild, Input, OnChanges, SimpleChange, EventEmitter, Output, OnInit} from '@angular/core'
 import {NgFor, NgClass, NgIf} from "@angular/common";
-import {CORE_DIRECTIVES} from "@angular/common"
+import {CORE_DIRECTIVES, FORM_DIRECTIVES} from "@angular/common"
 import {DataService} from './services/data'
 import {Data} from "./interface/interface";
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 
 @Component({
     selector: 'display',
-    directives: [CORE_DIRECTIVES, NgFor, NgClass, NgIf],
+    directives: [CORE_DIRECTIVES, NgFor, NgClass, NgIf, CHART_DIRECTIVES, FORM_DIRECTIVES],
     templateUrl: './app/display.component.html',
-    provider:[DataService]
+    providers: [DataService]
 })
 
 
 export class DisplayComponent implements OnInit {
     public data;
+
     getData(){
-        this.data = this.dataService.getData()
+        this.data = this.dataService.getData()[0]
     }
 
 
@@ -29,5 +31,27 @@ export class DisplayComponent implements OnInit {
 
     ngOnInit(){
         this.getData()
+    }
+
+    public barChartOptions:any = {
+        scaleShowVerticalLines: false,
+        responsive: true
+    };
+    public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public barChartType:string = 'bar';
+    public barChartLegend:boolean = true;
+
+    public barChartDatas:any[] = [
+        {data: [65, 59, 80, 81, 56, 55, 40], label:'Series A'},
+        {data: [28, 48, 40, 19, 86, 27, 90], label:'Series B'}
+    ];
+
+    // events
+    public chartClicked(e:any):void {
+        console.log(e);
+    }
+
+    public chartHovered(e:any):void {
+        console.log(e);
     }
 }
